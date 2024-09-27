5b5bb96f36cefc17887ba03bd4dc9468312a1424
Simran Dangol <simrandangol10@gmail.com>
2024-09-27 17:28:08 +0545

    ..

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 //routes import
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import listingRouter from "./routes/listing.routes.js";
 import uploadRouter from "./routes/upload.routes.js";
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -21,5 +24,11 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/listing", listingRouter);
 app.use("/api/v1/upload", uploadRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
 
 export { app };
